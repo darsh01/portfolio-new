0a4b2b382be9794ecd3f217b98f4314985fc3a89
darsh01 <dhmaniar01@gmail.com>
2024-06-19 12:07:37 -0700

    Deployment-9 12:07 PM

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title = "DM | Darsh Maniar"; // Set the document title
 root.render(
   <React.StrictMode>
     <App />
